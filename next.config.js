2b1768eeb7c4b9b9abfb59a5c1b1140419a19f2b
Hunter S <67876717+hunterschep@users.noreply.github.com>
2025-12-12 20:26:30 -0500

    rebuild

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  // This should match your GitHub repository name exactly
-  // e.g. if your repo is https://github.com/user/eastern-massage-spa, this should be '/eastern-massage-spa'
-  basePath: '/eastern-massage-spa',
-  images: {
-    unoptimized: true,
-  },
+  output: "export",
 };
+
+module.exports = nextConfig;
+
 
 module.exports = nextConfig;
